2a1e6be9f7824bb550e5ec6c181ce6b3964b905d
Ahmed Ayman <aelazaab@ntgclarity.com>
2023-08-09 10:29:35 +0300

    add missing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './component/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
-import { HomeComponent } from './component/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { AdminGuard } from './guards/admin.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, },
   {
-    path: 'stc-product', loadChildren: () => import('./component/product/product.module')
+    path: 'stc-product', loadChildren: () => import('./components/product/product.module')
       .then(m => m.ProductModule), canActivate: [AdminGuard]
   },
   {
-    path: 'stc-categories', loadChildren: () => import('./component/categories/categories.module')
+    path: 'stc-categories', loadChildren: () => import('./components/categories/categories.module')
       .then(m => m.CategoriesModule)
   },
   { path: 'stc-home', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './Common/material.module';
-import { NavbarComponent } from './component/navbar/navbar.component';
-import { LoginComponent } from './component/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
-import { HomeComponent } from './component/home/home.component';
-import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { CategoriesModule } from './component/categories/categories.module';
-import { ProductModule } from './component/product/product.module';
+import { CategoriesModule } from './components/categories/categories.module';
+import { ProductModule } from './components/product/product.module';
 
 @NgModule({
   declarations: [
